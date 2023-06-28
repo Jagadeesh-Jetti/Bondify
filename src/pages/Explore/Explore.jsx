7109b0469143850a39362cca189c6d51b962b89c
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-06-29 01:27:35 +0530

    feat: created explore page

---
+++ src/pages/Explore/Explore.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { LeftNavLayout } from "../../components/LayoutComponents/LeftNavLayout/LeftNavLayout";
+import { PostsLayout } from "../../components/LayoutComponents/PostsLayout/PostsLayout";
+import { RightSideLayout } from "../../components/LayoutComponents/RightSideLayout/RightSideLayout";
+import "../Explore/Explore.css";
+import { DataContext } from "../../contexts/DataContext";
+
+export const Explore = () => {
+  const { dataState } = useContext(DataContext);
+  return (
+    <div className="explore-container">
+      <LeftNavLayout />
+      <PostsLayout data={dataState?.posts} />
+      <RightSideLayout />
+    </div>
+  );
+};
