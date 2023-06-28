0d3c46d241c972b1e33ecbccefe079c2db095859
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-06-29 01:25:09 +0530

    feat: Right side layout

---
+++ src/components/LayoutComponents/RightSideLayout/RightSideLayout.jsx
@@ -0,0 +1,8 @@
+import "../RightSideLayout/RightSideLayout.css";
+export const RightSideLayout = () => {
+  return (
+    <div className="home-right-container">
+      this is the right find people container
+    </div>
+  );
+};
