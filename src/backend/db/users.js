628195bcfbf305186a7b0ca7a0a17794dca91f87
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-06-29 01:34:27 +0530

    update: updated the backend files

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -1,5 +1,6 @@
 import { v4 as uuid } from "uuid";
 import { formatDate } from "../utils/authUtils";
+
 /**
  * User Database can be added here.
  * You can add default users of your wish with different attributes
@@ -12,7 +13,98 @@ export const users = [
     lastName: "Balika",
     username: "adarshbalika",
     password: "adarshBalika123",
-    createdAt: formatDate(),
+    bio: "Be yourself!",
+    bookmarks: [],
+    avatarUrl:
+      "https://res.cloudinary.com/dtrjdcrme/image/upload/v1651473734/socialmedia/avatars/adarsh-balika_dct6gm.webp",
+    website: "https://romabulani.netlify.app/",
+    createdAt: "2022-01-01T10:55:06+05:30",
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    firstName: "Vikas",
+    lastName: "Lankalapalli",
+    username: "itsMeVikas",
+    password: "vikas123",
+    bio: "Hello World",
+    bookmarks: [],
+    avatarUrl:
+      "https://res.cloudinary.com/dtrjdcrme/image/upload/v1651554207/socialmedia/avatars/john-doe_gbkuda.webp",
+    website: "https://google.com/",
+    createdAt: "2022-01-02T10:55:06+05:30",
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    firstName: "Prahalad",
+    lastName: "Mattaparthi",
+    username: "heyPrahalad",
+    password: "prahalad123",
+    bio: "Whats in bio?",
+    bookmarks: [],
+    avatarUrl:
+      "https://res.cloudinary.com/dtrjdcrme/image/upload/v1651554256/socialmedia/avatars/jane-doe_il3cvx.webp",
+    website: "https://romabulani.netlify.app/",
+    createdAt: "2022-01-01T10:55:06+05:30",
+    updatedAt: formatDate(),
+  },
+  {
+    _id: uuid(),
+    firstName: "Vijay",
+    lastName: "Gurre",
+    username: "vijayKumar",
+    password: "vijay123",
+    bio: "Whats in bio?",
+    avatarUrl:
+      "https://res.cloudinary.com/dtrjdcrme/image/upload/v1651563581/socialmedia/avatars/carl-smith_mehw0u.webp",
+    website: "https://romabulani.netlify.app/",
+    createdAt: "2022-01-03T10:55:06+05:30",
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: uuid(),
+    firstName: "Srinivas",
+    lastName: "Kavi",
+    username: "srinivas",
+    password: "srinivas123",
+    bio: "Aspiring Frontend Engineer",
+    bookmarks: [],
+    avatarUrl:
+      "https://res.cloudinary.com/dtrjdcrme/image/upload/v1652525370/socialmedia/avatars/roma.webp",
+    website: "https://romabulani.hashnode.dev/",
+    createdAt: "2022-01-04T10:55:06+05:30",
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: uuid(),
+    firstName: "Madhu",
+    lastName: "Pulidhindi",
+    username: "madhu9143",
+    password: "madhu123",
+    bio: "What's up?",
+    bookmarks: [],
+    avatarUrl:
+      "https://res.cloudinary.com/dtrjdcrme/image/upload/v1652525373/socialmedia/avatars/alex-maxwell.webp",
+    website: "",
+    createdAt: "2022-01-05T10:55:06+05:30",
+    updatedAt: formatDate(),
+  },
+
+  {
+    _id: uuid(),
+    firstName: "Jay",
+    lastName: "Kumar",
+    username: "iamJay",
+    password: "Jay123",
+    bio: "Frontend Engineer",
+    bookmarks: [],
+    avatarUrl:
+      "https://res.cloudinary.com/dtrjdcrme/image/upload/v1652525510/socialmedia/avatars/sophia-jones.webp",
+    website: "",
+    createdAt: "2022-01-06T10:55:06+05:30",
     updatedAt: formatDate(),
   },
 ];
