33a16bb900d13bc2bb763772602d863ede659e5d
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-07-03 17:50:36 +0530

    refactor: updated the home content

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -4,23 +4,49 @@ import { RightSideLayout } from "../../components/LayoutComponents/RightSideLayo
 import { PostsLayout } from "../../components/LayoutComponents/PostsLayout/PostsLayout";
 import { useContext } from "react";
 import { DataContext } from "../../contexts/DataContext";
+import { DATAACTIONS } from "../../services/actions";
+import { Filtering } from "../../services/filtering";
 
 export const Home = () => {
-  const { dataState } = useContext(DataContext);
+  const { dataState, dataDispatch } = useContext(DataContext);
+  const updatedPosts = Filtering(dataState);
   return (
     <div className="home-main-container">
       <LeftNavLayout />
+
       <div className="home-middle-container">
         <div className="home-middle-header">
-          <div className="home-middle-header-title">
-            <h3>Home</h3>
-          </div>
-          <div className="home-middle-header-nav">
+          <div className="home-middle-header-title">Home</div>
+          {/* <div className="home-middle-header-nav">
             <h4> For you </h4>
             <h4> Following </h4>
-          </div>
-          <PostsLayout data={dataState.posts} />
+          </div> */}
         </div>
+        <div className="home-middle-header-filters">
+          <div
+            className="hmh-filters-trending"
+            onClick={() =>
+              dataDispatch({
+                type: DATAACTIONS.SETSORTTYPE,
+                payload: "trending",
+              })
+            }
+          >
+            Trending
+          </div>
+          <div
+            className="hmh-filters-oldest"
+            onClick={() =>
+              dataDispatch({
+                type: DATAACTIONS.SETSORTTYPE,
+                payload: "older",
+              })
+            }
+          >
+            Oldest
+          </div>
+        </div>
+        <PostsLayout data={updatedPosts} />
       </div>
       <RightSideLayout />
     </div>
