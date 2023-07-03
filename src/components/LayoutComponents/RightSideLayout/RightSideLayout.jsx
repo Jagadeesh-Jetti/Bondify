647ca64e5c35fcdb6f60d995caed9866b5c13864
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-07-03 17:48:46 +0530

    refactor: updated user suggestions

--- src/components/LayoutComponents/RightSideLayout/RightSideLayout.jsx
+++ src/components/LayoutComponents/RightSideLayout/RightSideLayout.jsx
@@ -1,8 +1,61 @@
+import { useContext } from "react";
+import { DataContext } from "../../../contexts/DataContext";
 import "../RightSideLayout/RightSideLayout.css";
+import { followHandler } from "../../../services/APIservices/FunctionalCalls/followHandler";
+import { unfollowHandler } from "../../../services/APIservices/FunctionalCalls/unfollowHandler";
+import { useNavigate } from "react-router-dom";
+
 export const RightSideLayout = () => {
+  const { dataState, dataDispatch } = useContext(DataContext);
+  const navigate = useNavigate();
+
+  const isFollowed = (userId) => {
+    return dataState.loggedInUser?.following?.some(({ _id }) => _id === userId);
+  };
+
   return (
     <div className="home-right-container">
-      this is the right find people container
+      <div className="rsl-search-container">
+        <input type="search" className="rsl-search" placeholder="Search" />
+      </div>
+      <div className="rsl-users-container">
+        <div className="rsl-users-container-name">
+          <h2> Who to follow </h2>
+        </div>
+        <div>
+          {dataState.users.map((user) => (
+            <div
+              className="rsl-usercard"
+              key={user._id}
+              onClick={() => navigate(`/profile/${user.username}`)}
+            >
+              <div className="rsl-usercard-dp-container">
+                <img
+                  src={user.avatarUrl}
+                  alt="loading"
+                  className="rsl-usercard-dp"
+                />
+              </div>
+              <div className="rsl-usercard-name">
+                <div>
+                  {user.firstName} {user.lastName}
+                </div>
+                <div> @{user.username}</div>
+              </div>
+              <div
+                className="rsl-usercard-follow"
+                onClick={() => {
+                  isFollowed(user._id)
+                    ? unfollowHandler(user._id, dataDispatch)
+                    : followHandler(user._id, dataDispatch);
+                }}
+              >
+                {isFollowed(user._id) ? "unfollowed" : "follow"}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
