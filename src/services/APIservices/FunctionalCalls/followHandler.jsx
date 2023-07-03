9a553a1807080bd0257ec3b65cfbe534c3766271
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-07-03 17:54:48 +0530

    feat: updated the follow and unfollow handlers

---
+++ src/services/APIservices/FunctionalCalls/followHandler.jsx
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { DATAACTIONS } from "../../actions";
+
+export const followHandler = async (userId, dataDispatch) => {
+  try {
+    const encodedToken = localStorage.getItem("token");
+    const response = await axios.post(
+      `/api/users/follow/${userId}`,
+      {},
+      {
+        headers: {
+          authorization: encodedToken,
+        },
+      }
+    );
+    dataDispatch({
+      type: DATAACTIONS.SETLOGGEDINUSER,
+      payload: response.data.user,
+    });
+    console.log(response.data.user);
+  } catch (error) {
+    console.error(error);
+  }
+};
