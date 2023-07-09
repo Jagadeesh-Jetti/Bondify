88e03fe8965e09edbf8e3a375e9774da0228a572
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2023-07-09 23:44:38 +0530

    feat: initialized create user feature

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import "../Home/home.css";
 import { LeftNavLayout } from "../../components/LayoutComponents/LeftNavLayout/LeftNavLayout";
 import { RightSideLayout } from "../../components/LayoutComponents/RightSideLayout/RightSideLayout";
@@ -10,6 +11,30 @@ import { Filtering } from "../../services/filtering";
 export const Home = () => {
   const { dataState, dataDispatch } = useContext(DataContext);
   const updatedPosts = Filtering(dataState);
+
+  const [postContent, setPostContent] = useState("");
+
+  const handlePostContentChange = (e) => {
+    setPostContent(e.target.value);
+  };
+
+  const handleCreatePost = () => {
+    // Handle logic to create a post with the postContent
+    // You can make an API call or dispatch an action to update the state
+
+    // Example using dataDispatch:
+    dataDispatch({
+      type: DATAACTIONS.CREATE_POST,
+      payload: {
+        content: postContent,
+        // Additional properties for the new post
+      },
+    });
+
+    // Reset the postContent state after creating the post
+    setPostContent("");
+  };
+
   return (
     <div className="home-main-container">
       <LeftNavLayout />
@@ -17,33 +42,38 @@ export const Home = () => {
       <div className="home-middle-container">
         <div className="home-middle-header">
           <div className="home-middle-header-title">Home</div>
-          {/* <div className="home-middle-header-nav">
-            <h4> For you </h4>
-            <h4> Following </h4>
-          </div> */}
         </div>
-        <div className="home-middle-header-filters">
-          <div
-            className="hmh-filters-trending"
-            onClick={() =>
-              dataDispatch({
-                type: DATAACTIONS.SETSORTTYPE,
-                payload: "trending",
-              })
-            }
-          >
-            Trending
+        <div className="home-middle-header-filters">{/* Filters code */}</div>
+        <div className="hml-create-post">
+          <div className="hml-dp-container">
+            <img
+              src={dataState.loggedInUser.avatarUrl}
+              alt="user-dp"
+              className="hml-dp"
+            />
           </div>
-          <div
-            className="hmh-filters-oldest"
-            onClick={() =>
-              dataDispatch({
-                type: DATAACTIONS.SETSORTTYPE,
-                payload: "older",
-              })
-            }
-          >
-            Oldest
+          <div className="hml-create-post-input">
+            <textarea
+              value={postContent}
+              onChange={handlePostContentChange}
+              placeholder="What's happening?"
+              maxLength={280}
+              className="hml-create-post-textarea"
+            />
+            <div className="hml-create-post-footer">
+              <div className="hml-create-post-counter">
+                {280 - postContent.length}
+              </div>
+              <button
+                className={`hml-create-post-button ${
+                  postContent.length === 0 ? "disabled" : ""
+                }`}
+                onClick={handleCreatePost}
+                disabled={postContent.length === 0}
+              >
+                Tweet
+              </button>
+            </div>
           </div>
         </div>
         <PostsLayout data={updatedPosts} />
