89c982ed8eb6047b4c1b732374e65e0791587b7e
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-03-06 23:52:50 +0530

    feat: made search functional

--- src/components/LayoutComponents/RightSideLayout/RightSideLayout.jsx
+++ src/components/LayoutComponents/RightSideLayout/RightSideLayout.jsx
@@ -1,63 +1,71 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { DataContext } from "../../../contexts/DataContext";
 import "../RightSideLayout/RightSideLayout.css";
-import { followHandler } from "../../../services/APIservices/FunctionalCalls/followHandler";
-import { unfollowHandler } from "../../../services/APIservices/FunctionalCalls/unfollowHandler";
-import { useNavigate } from "react-router-dom";
+import { SearchResults } from "../../SearchResults/SearchResults";
+import { UserCard } from "../../UserCard/UserCard";
+import { FaSearch, FaTimesCircle } from "react-icons/fa";
 
 export const RightSideLayout = () => {
-  const { dataState, dataDispatch } = useContext(DataContext);
-  const navigate = useNavigate();
-
-  const isFollowed = (userId) => {
-    return dataState.loggedInUser?.following?.some(({ _id }) => _id === userId);
-  };
+  const [input, setInput] = useState("");
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const { dataState } = useContext(DataContext);
 
   const usersToShow = dataState.users.filter(
     (user) => user.username !== dataState.loggedInUser.username
   );
+
+  const getFilteredUsers = (input) => {
+    const filteredData = dataState.users.filter(
+      (user) =>
+        user.lastName.toLowerCase().includes(input.toLowerCase()) ||
+        user.firstName.toLowerCase().includes(input.toLowerCase())
+    );
+    setFilteredUsers(filteredData);
+  };
+
+  const handleInputChange = (input) => {
+    setInput(input);
+    getFilteredUsers(input);
+  };
+
+  const handleClearSearch = () => {
+    setInput("");
+    setFilteredUsers([]);
+  };
+
   return (
     <div className="home-right-container">
       <div className="rsl-search-container">
-        <input type="search" className="rsl-search" placeholder="Search" />
+        <div className="rsl-search-input-display">
+          <FaSearch id="search-icon" />
+          <input
+            type="search"
+            value={input}
+            className="rsl-search"
+            placeholder="Search"
+            onChange={(e) => handleInputChange(e.target.value)}
+          />
+          {input && (
+            <FaTimesCircle
+              id="clear-search-icon"
+              onClick={handleClearSearch}
+              className="clear-search-icon"
+            />
+          )}
+        </div>
+
+        {input && filteredUsers.length > 0 && (
+          <SearchResults filteredUsers={filteredUsers} />
+        )}
       </div>
+
       <div className="rsl-users-container">
         <div className="rsl-users-container-name">
-          <h2> Who to follow </h2>
+          <h3> Who to follow </h3>
         </div>
         <div>
           {usersToShow.map((user) => (
-            <div className="rsl-usercard" key={user._id}>
-              <div
-                className="rsl-usercard-dp-container"
-                onClick={() => navigate(`/profile/${user.username}`)}
-              >
-                <img
-                  src={user.avatarUrl}
-                  alt="loading"
-                  className="rsl-usercard-dp"
-                />
-              </div>
-              <div
-                className="rsl-usercard-name"
-                onClick={() => navigate(`/profile/${user.username}`)}
-              >
-                <div>
-                  {user.firstName} {user.lastName}
-                </div>
-                <div> @{user.username}</div>
-              </div>
-              <div
-                className="rsl-usercard-follow"
-                onClick={() => {
-                  isFollowed(user._id)
-                    ? unfollowHandler(user._id, dataDispatch)
-                    : followHandler(user._id, dataDispatch);
-                }}
-              >
-                {isFollowed(user._id) ? "Unfollow" : "Follow"}
-              </div>
-            </div>
+            <UserCard user={user} followOption={true} key={user._id} />
           ))}
         </div>
       </div>
