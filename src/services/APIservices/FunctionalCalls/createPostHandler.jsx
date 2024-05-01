b5fee17170d06f2aa99b6ba066daac0e0ae1d5df
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-05-01 20:40:26 +0530

    refactor: cleaned up unnecessary imports

--- src/services/APIservices/FunctionalCalls/createPostHandler.jsx
+++ src/services/APIservices/FunctionalCalls/createPostHandler.jsx
@@ -1,10 +1,7 @@
 import axios from "axios";
 import { DATAACTIONS } from "../../actions";
-// import { useContext } from "react";
-// import { DataContext } from "../../../contexts/DataContext";
 
 export const createPostService = async (post, dataDispatch, setNewPost) => {
-  //   const { setNewPost } = useContext(DataContext);
   try {
     const encodedToken = localStorage.getItem("token");
     const response = await axios.post(
