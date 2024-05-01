d8c0bf2ee544f8c461f0c498ffe1e85c17ea27cc
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-05-01 20:36:11 +0530

    refactor: cleaned up consoles

--- src/services/APIservices/FunctionalCalls/createPostHandler.jsx
+++ src/services/APIservices/FunctionalCalls/createPostHandler.jsx
@@ -12,7 +12,7 @@ export const createPostService = async (post, dataDispatch, setNewPost) => {
       { postData: post },
       { headers: { authorization: encodedToken } }
     );
-
+    console.log(response.data.posts);
     dataDispatch({
       type: DATAACTIONS.SETPOSTS,
       payload: response.data.posts,
